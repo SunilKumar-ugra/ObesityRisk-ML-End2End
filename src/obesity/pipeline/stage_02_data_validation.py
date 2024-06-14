74ac869830583306a89c84264a682ba829135f0a
SunilKumar-ugra <ugargolsunilkumar@gmail.com>
2024-06-14 13:28:47 +0530

    pipeline - valid

---
+++ src/obesity/pipeline/stage_02_data_validation.py
@@ -0,0 +1,11 @@
+from obesity.config import ConfigurationManager
+from obesity.components.data_validation import DataValidation
+
+class DataValidationTrainingPipeline:
+    def __init__(self):
+        pass
+    def main(self):
+        config = ConfigurationManager()
+        data_validation_config = config.get_data_validation_config()
+        data_validation = DataValidation(config=data_validation_config)
+        data_validation.validate_all_columns()
