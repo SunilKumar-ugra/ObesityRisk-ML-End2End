88216493e3949db98824ba6ca0fa0796db0fca58
SunilKumar-ugra <ugargolsunilkumar@gmail.com>
2024-06-25 23:20:10 +0530

    pipeline-transform

---
+++ src/obesity/pipeline/stage_03_data_transformation.py
@@ -0,0 +1,13 @@
+from obesity.config import ConfigurationManager
+from obesity.components.data_transformation import DataTransformation
+
+
+class DataTransformationTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        data_transformation_config = config.get_data_transformation_config()
+        data_transformation = DataTransformation(config=data_transformation_config)
+        data_transformation.transformation()
