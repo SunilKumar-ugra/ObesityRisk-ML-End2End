ab2a88d10daef4a7d83a3230ae87d541a56abc7b
SunilKumar-ugra <ugargolsunilkumar@gmail.com>
2024-07-01 12:19:51 +0530

    Main-Transform

--- main.py
+++ main.py
@@ -24,3 +24,15 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e
+
+from obesity.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+
+STAGE_NAME = "Data Transformation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_transformation = DataTransformationTrainingPipeline()
+   data_transformation.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
