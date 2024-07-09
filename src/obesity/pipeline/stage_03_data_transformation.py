b0e983d34b01ad91e1737854bad85922be3cb359
SunilKumar-ugra <ugargolsunilkumar@gmail.com>
2024-07-09 19:04:38 +0530

    Transformation-pipeline

--- src/obesity/pipeline/stage_03_data_transformation.py
+++ src/obesity/pipeline/stage_03_data_transformation.py
@@ -1,3 +1,4 @@
+from obesity import logger 
 from obesity.config import ConfigurationManager
 from obesity.components.data_transformation import DataTransformation
 
@@ -11,3 +12,14 @@ class DataTransformationTrainingPipeline:
         data_transformation_config = config.get_data_transformation_config()
         data_transformation = DataTransformation(config=data_transformation_config)
         data_transformation.transformation()
+
+if __name__ == '__main__':
+    STAGE_NAME = "Data Transformation stage"
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+        data_transformation = DataTransformationTrainingPipeline()
+        data_transformation.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+            logger.exception(e)
+            raise e
