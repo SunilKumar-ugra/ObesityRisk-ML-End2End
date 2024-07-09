158fc8e873ef900cbe4044cfa7afd082f7432c2d
SunilKumar-ugra <ugargolsunilkumar@gmail.com>
2024-07-09 19:18:24 +0530

    modelTrainer-Pipeline

--- src/obesity/pipeline/stage_04_model_trainer.py
+++ src/obesity/pipeline/stage_04_model_trainer.py
@@ -1,3 +1,4 @@
+from obesity import logger  
 from obesity.config import ConfigurationManager
 from obesity.components.model_trainer import ModelTrainer
 
@@ -12,3 +13,15 @@ class ModelTrainerTrainingPipeline:
         model_trainer_config = config.get_model_trainer_config()
         model_trainer_config = ModelTrainer(config=model_trainer_config)
         model_trainer_config.train()
+
+if __name__ == '__main__':
+    STAGE_NAME = "Model Trainer stage"
+    try: 
+        logger.info(f"*******************")
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        model_trainer = ModelTrainerTrainingPipeline()
+        model_trainer.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+            logger.exception(e)
+            raise e
