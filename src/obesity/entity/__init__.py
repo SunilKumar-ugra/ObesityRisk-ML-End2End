4eb3edf92b6b2f7725c078844e999e664b214ab5
SunilKumar-ugra <ugargolsunilkumar@gmail.com>
2024-07-09 17:53:26 +0530

    Entity-Evaluation

--- src/obesity/entity/__init__.py
+++ src/obesity/entity/__init__.py
@@ -33,3 +33,15 @@ class ModelTrainerConfig:
     test_data_path: Path
     model_name: str
     target_column: str
+
+
+@dataclass(frozen=True)
+class ModelEvaluationConfig:
+    root_dir: Path
+    train_data_path: Path
+    test_data_path: Path
+    model_path: Path
+    all_params: dict
+    metric_file_name: Path
+    target_column: str
+    mlflow_uri: str
